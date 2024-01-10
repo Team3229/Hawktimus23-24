691ddb87bad82eb44ef4fd62c51a0c9bbd2d5d68
LegoNick1208 <nshreve@outlook.com>
2024-01-10 14:30:02 -0500

    Refactored Repo

---
+++ Outreach Bot/src/main/java/frc/robot/Drivetrain/SwerveKinematics.java
@@ -0,0 +1,238 @@
+package frc.robot.Drivetrain;
+
+import com.kauailabs.navx.frc.AHRS;
+import com.pathplanner.lib.util.PIDConstants;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.SPI;
+
+public class SwerveKinematics {
+
+    // Swerve Modules
+    public static SwerveModule frontLeftModule;
+    public static SwerveModule frontRightModule;
+    public static SwerveModule backLeftModule;
+    public static SwerveModule backRightModule;
+    // public static ModuleOffsets offsets;
+
+    /**Array of the angle offsets for each swerve module. (Edit the values here to your needs)*/
+    private static Rotation2d[] moduleOffsets = {Rotation2d.fromDegrees(-285.205078125), Rotation2d.fromDegrees(-350.947265625), Rotation2d.fromDegrees(-317.72460937500006), Rotation2d.fromDegrees(-272.021484375)};
+
+    /**PID values for the swerve modules' angular motion. (Automatically populated with our constants we used for the 22-23 season) */
+    private static final PIDConstants anglePID = new PIDConstants(0.009, 0, 0);
+    // private static final double[] anglePID = {0.01, 0.0001, 0};
+    /**PID values for the swerve modules' driving motion. (Automatically populated with our constants we used for the 22-23 season) */
+    private static final PIDConstants drivePID = new PIDConstants(0.1, 0, 0);
+    // private static final double[] drivePID = {0.1, 0, 0};
+
+    /**Kauai Labs NavX Gyro. */
+    public static AHRS navxGyro;
+    /**The current rotation of the chassis. */
+    public static Rotation2d robotRotation = Rotation2d.fromDegrees(0);
+    /**
+     * Robot relative mode uses the current rotation of the robot as forward, instead of the default
+     * field-relative mode. This is useful for picking up objects or lining up with a game piece.
+     */
+    private static boolean relativeMode;
+
+    /**An object used to calculate module velocities from overall chassis movement. */
+    public static SwerveDriveKinematics kinematics;
+    /**The current state of chassis velocity. */
+    private static ChassisSpeeds chassisState;
+    /**The current state of chassis velocity. relative to bot */
+    private static ChassisSpeeds chassisRelativeState;
+    /**The current set module states. */
+    private static SwerveModuleState[] moduleStates;
+    /**The current set module positions. */
+    public static SwerveModulePosition[] modulePositions = {new SwerveModulePosition(), new SwerveModulePosition(), new SwerveModulePosition(), new SwerveModulePosition()};
+
+    /**The width of the robot chassis in meters. */
+    public static final double robotWidth = 0.6858;
+    //TODO: Measure and set this constant
+    //**The distance between the edge of the chassis to the center of a wheel in meters. */
+    public static final double moduleEdgeOffset = 0.0508;
+    /**The maximum speed (in meters/sec) that a singular swerve module can reach. */
+    private static final double maxModuleSpeed = 20;
+    /**The maximum linear speed (in meters/sec) the chassis should move at. (Automatically set for SDS MK4 L1 modules) */
+    private static final double chassisSpeed = 5;
+    /**The maximum angular speed (in radians/sec) that the chassis can rotate at. */
+    public static double maxChassisRotationSpeed = 5;
+    /**Whether or not to run the drive motors in brake mode. */
+    private static final boolean brakeMode = false;
+    
+    public SwerveKinematics() {}
+
+    /**Initializes the drivetrain. */
+    public static void initialize() {
+
+        // Replace the CAN IDs to suit your needs.
+        frontLeftModule = new SwerveModule(2, 3, 1, new Translation2d((robotWidth/2), (robotWidth/2)));
+        frontRightModule = new SwerveModule(14, 15, 16, new Translation2d((robotWidth/2), -(robotWidth/2)));
+        backLeftModule = new SwerveModule(6, 7, 8, new Translation2d(-(robotWidth/2), (robotWidth/2)));
+        backRightModule = new SwerveModule(10, 11, 9, new Translation2d(-(robotWidth/2), -(robotWidth/2)));
+
+        // offsets = new ModuleOffsets();
+        // configOffsets(offsets.read());
+
+        navxGyro = new AHRS(SPI.Port.kMXP);
+
+        kinematics = new SwerveDriveKinematics(frontLeftModule.location, frontRightModule.location, backLeftModule.location, backRightModule.location);
+
+        configureEncoders();
+        configureMotors();
+        configurePID();
+
+        modulePositions[0] = new SwerveModulePosition();
+        modulePositions[1] = new SwerveModulePosition();
+        modulePositions[2] = new SwerveModulePosition();
+        modulePositions[3] = new SwerveModulePosition();
+    }
+
+    public static void configOffsets(){
+        // Rotation2d[] offset = offsets.read();
+        frontLeftModule.doOffset(moduleOffsets[0]);
+        frontRightModule.doOffset(moduleOffsets[1]);
+        backLeftModule.doOffset(moduleOffsets[2]);
+        backRightModule.doOffset(moduleOffsets[3]);
+    }
+
+    public static void configOffsets(Rotation2d[] offset){
+        moduleOffsets = offset;
+        frontLeftModule.doOffset(offset[0]);
+        frontRightModule.doOffset(offset[1]);
+        backLeftModule.doOffset(offset[2]);
+        backRightModule.doOffset(offset[3]);
+    }
+    /**
+     * Drives the chassis given an x, y, and rotational movement speed.
+     * @param X X axis speed (left/right relative to driverstation)
+     * @param Y Y axis speed (forward/backward relative to driverstation)
+     * @param Z Rotational speed
+     */
+    public static void drive(double X, double Y, double Z) {
+
+        // If we are in robot relative mode, override the chassis rotation to 180 degrees.
+        if (relativeMode) {
+            robotRotation = Rotation2d.fromDegrees(180);
+        } else {
+            robotRotation = Rotation2d.fromDegrees(MathUtil.inputModulus(-navxGyro.getYaw(), 0, 360));
+        }
+
+        // Calculate reverse kinematics
+        chassisState = ChassisSpeeds.fromFieldRelativeSpeeds(Y*chassisSpeed, X*chassisSpeed, Z*maxChassisRotationSpeed, robotRotation);
+        chassisRelativeState = ChassisSpeeds.fromFieldRelativeSpeeds(Y*chassisSpeed, X*chassisSpeed, Z*maxChassisRotationSpeed, Rotation2d.fromDegrees(180));
+        
+        moduleStates = kinematics.toSwerveModuleStates(chassisState);
+        SwerveDriveKinematics.desaturateWheelSpeeds(moduleStates, maxModuleSpeed);
+
+        // Set each module state
+        frontLeftModule.setModuleState(moduleStates[0]);
+        frontRightModule.setModuleState(moduleStates[1]);
+        backLeftModule.setModuleState(moduleStates[2]);
+        backRightModule.setModuleState(moduleStates[3]);
+
+        frontLeftModule.updateModuleState();
+        frontRightModule.updateModuleState();
+        backLeftModule.updateModuleState();
+        backRightModule.updateModuleState();
+
+        frontLeftModule.updateModulePosition();
+        frontRightModule.updateModulePosition();
+        backLeftModule.updateModulePosition();
+        backRightModule.updateModulePosition();
+
+        modulePositions[0] = frontLeftModule.currentPosition;
+        modulePositions[1] = frontRightModule.currentPosition;
+        modulePositions[2] = backLeftModule.currentPosition;
+        modulePositions[3] = backRightModule.currentPosition;
+    }
+
+    /**
+     * Drives the chassis given a chassis relative speed
+     * @param speeds (ChassisSpeeds)
+     */
+    public static void drive(ChassisSpeeds speeds) {
+
+        // Calculate reverse kinematics
+        chassisState = ChassisSpeeds.fromFieldRelativeSpeeds(speeds, robotRotation);
+        chassisRelativeState = speeds;
+        
+        moduleStates = kinematics.toSwerveModuleStates(chassisState);
+        SwerveDriveKinematics.desaturateWheelSpeeds(moduleStates, maxModuleSpeed);
+
+        // Set each module state
+        frontLeftModule.setModuleState(moduleStates[0]);
+        frontRightModule.setModuleState(moduleStates[1]);
+        backLeftModule.setModuleState(moduleStates[2]);
+        backRightModule.setModuleState(moduleStates[3]);
+
+        frontLeftModule.updateModuleState();
+        frontRightModule.updateModuleState();
+        backLeftModule.updateModuleState();
+        backRightModule.updateModuleState();
+
+        frontLeftModule.updateModulePosition();
+        frontRightModule.updateModulePosition();
+        backLeftModule.updateModulePosition();
+        backRightModule.updateModulePosition();
+
+        modulePositions[0] = frontLeftModule.currentPosition;
+        modulePositions[1] = frontRightModule.currentPosition;
+        modulePositions[2] = backLeftModule.currentPosition;
+        modulePositions[3] = backRightModule.currentPosition;
+    }
+
+    public static ChassisSpeeds getSpeeds() {
+        return chassisRelativeState;
+    }
+
+    public static void zeroGyro() {
+        navxGyro.zeroYaw();
+    }
+
+    /**Configures each module's motors. */
+    public static void configureMotors() {
+
+        frontLeftModule.configureAngleMotor();
+        frontLeftModule.configureDriveMotor(brakeMode, false);
+        frontRightModule.configureAngleMotor();
+        frontRightModule.configureDriveMotor(brakeMode, false);
+        backLeftModule.configureAngleMotor();
+        backLeftModule.configureDriveMotor(brakeMode, false);
+        backRightModule.configureAngleMotor();
+        backRightModule.configureDriveMotor(brakeMode, true);
+    }
+
+    /**Configures and resets the offsets of each module's encoders. */
+    public static void configureEncoders() {
+        frontLeftModule.configureEncoders(moduleOffsets[0]);
+        frontRightModule.configureEncoders(moduleOffsets[1]);
+        backLeftModule.configureEncoders(moduleOffsets[2]);
+        backRightModule.configureEncoders(moduleOffsets[3]);
+    }
+
+    /**Configures each module's PID Controllers with the provided constants at the top of this class. */
+    public static void configurePID() {
+
+        frontLeftModule.configurePID(anglePID, drivePID);
+        frontRightModule.configurePID(anglePID, drivePID);
+        backLeftModule.configurePID(anglePID, drivePID);
+        backRightModule.configurePID(anglePID, drivePID);
+    }
+
+    /**Stops every module's motors. (should not be called during driving; the motors should continue to their setpoints) */
+    public static void stop() {
+
+        frontLeftModule.stopMotors();
+        frontRightModule.stopMotors();
+        backLeftModule.stopMotors();
+        backRightModule.stopMotors();
+    }
+
+}
---
+++ Outreach Bot/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,181 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.pathplanner.lib.auto.NamedCommands;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Inputs.Controller;
+import frc.robot.Inputs.Controller.ControllerType;
+import frc.robot.Inputs.Controller.Controls;
+import frc.robot.Vision.Limelight;
+import frc.robot.Autonomous.PathPlanner;
+import frc.robot.Drivetrain.SwerveKinematics;
+import frc.robot.Drivetrain.SwerveOdometry;
+	
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+
+	Controller flightStick;
+	Controller xboxController;
+
+	Command autoCommand;
+	PathPlanner autoManager;
+
+	Limelight limelight;
+
+	double[] desiredSwerveState = {0,0,0,0,0,0,0,0};
+	double[] measuredSwerveState = {0,0,0,0,0,0,0,0};
+
+	/**
+	 * This function is run when the robot is first started up and should be used for any
+	 * initialization code.
+	 */
+	@Override
+	public void robotInit() {
+
+		flightStick = new Controller(ControllerType.FlightStick, 0);
+		xboxController = new Controller(ControllerType.XboxController, 1);
+
+		autoManager = new PathPlanner();
+
+		limelight = new Limelight();
+
+		SwerveKinematics.initialize();
+
+		SwerveOdometry.initialize(limelight.getPose());
+
+	}
+
+	/**
+	 * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+	 * that you want ran during disabled, autonomous, teleoperated and test.
+	 *
+	 * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+	 * SmartDashboard integrated updating.
+	 */
+	@Override
+	public void robotPeriodic() {
+		logging();
+	}
+
+	/** This function is called once when autonomous is enabled. */
+	@Override
+	public void autonomousInit() {
+
+		SwerveKinematics.navxGyro.zeroYaw();
+
+		NamedCommands.registerCommand("doshoot", new Command() {});
+
+		autoCommand = autoManager.getCommand("Example Path");
+
+		autoCommand.initialize();
+		
+	}
+
+	/** This function is called periodically during autonomous. */
+	@Override
+	public void autonomousPeriodic() {
+
+		SwerveOdometry.addSensorData(new Pose2d(limelight.getPose().getX(), limelight.getPose().getY(), SwerveKinematics.robotRotation));
+		SwerveOdometry.update(SwerveKinematics.robotRotation, SwerveKinematics.modulePositions);
+
+		autoCommand.execute();
+	}
+
+	/** This function is called once when teleop is enabled. */
+	@Override
+	public void teleopInit() {
+
+		// Remove for Comp
+		SwerveKinematics.navxGyro.zeroYaw();
+
+		flightStick.nullControls();
+		xboxController.nullControls();
+
+		SwerveKinematics.configureEncoders();
+		SwerveKinematics.configureMotors();
+		SwerveKinematics.configurePID();
+
+		SwerveKinematics.configOffsets();
+	}
+
+	/** This function is called periodically during operator control. */
+	@Override
+	public void teleopPeriodic() {
+
+		flightStick.update();
+		// xboxController.update();
+
+		SwerveKinematics.maxChassisRotationSpeed = 10 * 0.5 * ((-(double) flightStick.get(Controls.FlightStick.Throttle)) + 1);
+
+		SwerveKinematics.drive(
+						(double) flightStick.get(Controls.FlightStick.AxisX),
+						(double) flightStick.get(Controls.FlightStick.AxisY),
+						(double) flightStick.get(Controls.FlightStick.AxisZ)
+					);
+
+		SwerveOdometry.addSensorData(limelight.getPose());
+		SwerveOdometry.update(SwerveKinematics.robotRotation, SwerveKinematics.modulePositions);
+
+		SmartDashboard.putNumberArray("odometry", new double[] {
+			SwerveOdometry.getPose().getX(),
+			SwerveOdometry.getPose().getY(),
+			SwerveOdometry.getPose().getRotation().getDegrees()
+		});
+
+		// logging();
+
+	}
+
+	/** This function is called once when the robot is disabled. */
+	@Override
+	public void disabledInit() {}
+
+	/** This function is called periodically when disabled. */
+	@Override
+	public void disabledPeriodic() {}
+
+	/** This function is called once when test mode is enabled. */
+	@Override
+	public void testInit() {}
+
+	/** This function is called periodically during test mode. */
+	@Override
+	public void testPeriodic() {}
+
+	/** This function is called once when the robot is first started up. */
+	@Override
+	public void simulationInit() {}
+
+	/** This function is called periodically whilst in simulation. */
+	@Override
+	public void simulationPeriodic() {}
+
+	public void logging() {
+
+		measuredSwerveState[0] = SwerveKinematics.frontLeftModule.getPosition().getDegrees();
+		measuredSwerveState[1] = -SwerveKinematics.frontLeftModule.getVelocity();
+		measuredSwerveState[2] = SwerveKinematics.frontRightModule.getPosition().getDegrees();
+		measuredSwerveState[3] = -SwerveKinematics.frontRightModule.getVelocity();
+		measuredSwerveState[4] = SwerveKinematics.backLeftModule.getPosition().getDegrees();
+		measuredSwerveState[5] = -SwerveKinematics.backLeftModule.getVelocity();
+		measuredSwerveState[6] = SwerveKinematics.backRightModule.getPosition().getDegrees();
+		measuredSwerveState[7] = -SwerveKinematics.backRightModule.getVelocity();
+		
+		SmartDashboard.putNumberArray("measuredSwerveState", measuredSwerveState);
+
+		SmartDashboard.putNumber("navX", SwerveKinematics.robotRotation.getDegrees());
+
+	}
+}
