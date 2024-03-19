6aa7ff4011e93d6d88388d339086a1ec3ce49150
Nathan <98428552+Xolverr@users.noreply.github.com>
2024-03-19 10:53:52 -0400

    add 3 note test

---
+++ src/main/java/frc/robot/Subsystems/Drivetrain/DrivetrainCommands.java
@@ -0,0 +1,14 @@
+package frc.robot.Subsystems.Drivetrain;
+
+import frc.robot.CommandsV2.Command;
+
+public class DrivetrainCommands {
+    
+    public static Command rotateSpeaker = new Command() {
+        @Override
+        public void periodic() {
+            
+        }
+    };
+
+}
