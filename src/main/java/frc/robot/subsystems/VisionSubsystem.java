0f9fc4ea104ddc2e44df921be33edcbdd64113ee
Nathan <98428552+Xolverr@users.noreply.github.com>
2024-10-18 08:38:07 -0400

    move limelight handling to VisionSubsystem.java

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,11 +1,21 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.LimelightHelpers;
+import frc.robot.LimelightHelpers.PoseEstimate;
 
-public class VisionSubsystem extends SubsystemBase {
-    
-    public VisionSubsystem() {
-        
+public class VisionSubsystem {
+
+    public static PoseEstimate getMT2Pose(Rotation2d robotRotation, double robotRotationRate) {
+        LimelightHelpers.SetRobotOrientation("limelight", robotRotation.getDegrees(), 0, 0, 0, 0, 0);
+
+        PoseEstimate mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
+
+        if (mt2 != null && Math.abs(robotRotationRate) < 720 && mt2.tagCount != 0) {
+            return mt2;
+        } else {
+            return null;
+        }
     }
 
 }
